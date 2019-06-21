7e7b279099aaea5cdcfc4be32b81dd56a2e0d76c
Ismail KABOUBI <ikaboubi@gmail.com>
2019-06-21 11:24:42 +0200

    delete minikube part

--- tools/kind-for-travis.sh
+++ tools/kind-for-travis.sh
@@ -3,25 +3,6 @@
 set -eu
 set -x
 
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v${KUBERNETES_VERSION}/bin/linux/amd64/kubectl
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin/
-
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v${MINIKUBE_VERSION}/minikube-linux-amd64
-chmod +x minikube
-sudo mv minikube /usr/local/bin/
-
-mkdir -p $HOME/.kube $HOME/.minikube
-touch $KUBECONFIG
-
-sudo minikube start \
-    --vm-driver=none \
-    --extra-config=apiserver.authorization-mode=RBAC \
-    --extra-config=apiserver.runtime-config=events.k8s.io/v1beta1=false \
-    --kubernetes-version=v${KUBERNETES_VERSION}
-
-sudo chown -R travis: /home/travis/.minikube/
-
 # Download and install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBERNETES_VERSION}/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
